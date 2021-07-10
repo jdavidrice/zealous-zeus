213fe2a1c80c21327831b90b20b5e42bfa19cff0
David Rodriguez <david@bsdadm.com>
2021-07-10 10:37:07 -0400

    add Third Party Authentication for future use

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,44 +1,66 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  role: {
+const ThirdPartyProviderSchema = new Schema({
+  provider_name: {
     type: String,
-    enum: ['ADMIN', 'USER'],
-    default: 'USER',
-    required: true,
+    default: null,
   },
-  username: {
+  provider_id: {
     type: String,
-    trim: true,
-    required: true,
-    index: { unique: true },
+    default: null,
   },
-  firstName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  password: {
-    type: String,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  memberSince: {
-    type: Date,
-    default: Date.now,
+  provider_data: {
+    type: {},
+    default: null,
   },
 });
 
-userSchema.set('timestamps', true);
+const userSchema = new Schema(
+  {
+    role: {
+      type: String,
+      enum: ['ADMIN', 'USER'],
+      default: 'USER',
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      index: { unique: true },
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    emailIsVerified: {
+      type: Boolean,
+      default: false,
+    },
+    memberSince: {
+      type: Date,
+      default: Date.now,
+    },
+    thirdPartyAuth: [ThirdPartyProviderSchema],
+  },
+  { strict: false },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
